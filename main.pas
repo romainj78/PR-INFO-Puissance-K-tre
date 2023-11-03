49a535ff63bc1451db2334d8ac3976b373d4bc25
JOLLY Romain <romainjolly78@gmail.com>
2023-11-03 14:31:18 +0100

    base création fenetre sdl

--- main.pas
+++ main.pas
@@ -15,11 +15,60 @@ uses
   logique_modeSurprise in 'jeu/logique_modeSurprise.pas',
   logique_modeSolo in 'jeu/logique_modeSolo.pas';
 
+var
+  sdlWindow1: PSDL_Window;
+  sdlRenderer: PSDL_Renderer;
+  sdlSurface1: PSDL_Surface;
+  sdlTexture1: PSDL_Texture;
+  sdlRectangle: TSDL_Rect;
+
 begin
   writeln('Hello World!');
 
   choixMode();
   choixN();
   jouer();
+
+  //initilization of video subsystem
+  if SDL_Init(SDL_INIT_VIDEO) < 0 then Halt;
+
+  if SDL_CreateWindowAndRenderer(500, 500, SDL_WINDOW_SHOWN, @sdlWindow1, @sdlRenderer) <> 0
+    then Halt;
+
+  // set scaling quality
+  SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, 'nearest');
+
+  // create surface from file
+  sdlSurface1 := SDL_LoadBMP('fpsdl.bmp');
+  if sdlSurface1 = nil then
+    Halt;
+
+  // load image file
+  sdlTexture1 := SDL_CreateTextureFromSurface(sdlRenderer, sdlSurface1);
+  if sdlTexture1 = nil then
+    Halt;
+
+  // prepare rectangle
+  sdlRectangle.x := 12;
+  sdlRectangle.y := 25;
+  sdlRectangle.w := 178;
+  sdlRectangle.h := 116;
+
+  // render texture
+  SDL_RenderCopy(sdlRenderer, sdlTexture1, @sdlRectangle, nil);
+  SDL_RenderCopy(sdlRenderer, sdlTexture1, nil, @sdlRectangle);
+
+  // render to window for 2 seconds
+  SDL_RenderPresent(sdlRenderer);
+  SDL_Delay(2000);
+
+  // clear memory
+  SDL_DestroyTexture(sdlTexture1);
+  SDL_FreeSurface(sdlSurface1);
+  SDL_DestroyRenderer(sdlRenderer);
+  SDL_DestroyWindow (sdlWindow1);
+
+  //closing SDL2
+  SDL_Quit;
 
 end.  
